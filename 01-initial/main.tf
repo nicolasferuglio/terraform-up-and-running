7ccb4156f99cf4bf5e0972bc2212afe0c1419203
Nicolas Feruglio <nicolas.feruglio@gmail.com>
2022-08-31 15:45:20 -0300

    first deployment: machine, security group, input var and output var

--- 01-initial/main.tf
+++ 01-initial/main.tf
@@ -3,10 +3,49 @@ provider "aws" {
 }
 
 resource "aws_instance" "first_instance" {
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.port} &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
-    Name = "my_machine"
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+
+
+  ingress {
+    description      = "Http from VPC"
+    from_port        = var.port
+    to_port          = var.port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+variable "port" {
+  type        = number
+  description = "Port fo ingress"
+  default     = 8080
+}
+
+output "public_ip" {
+    value = aws_instance.first_instance.public_ip
+    description = "The public IP to send requests"
+  
 }
